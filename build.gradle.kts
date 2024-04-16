9e9ceec30c9eeb843c9e7d1d64977a5026c0156e
blue42 <blue42.david@gmail.com>
2024-04-16 23:48:28 +0200

    Implemented basic security

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,9 +13,17 @@ repositories {
 }
 
 dependencies {
-    // mine:
+    // TODO 1. spring sec:
+    // makes everything "private" (which is good)
+    // login with user & password (generated in console)
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    // TODO 5. spring oauth2:
+    // oauth2 for SSO
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    // spring:
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
     // default:
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
