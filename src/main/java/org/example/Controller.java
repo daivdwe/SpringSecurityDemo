202ea9b8645648e4d8c1daf040490dee7290a0b6
blue42 <blue42.david@gmail.com>
2024-04-16 23:48:28 +0200

    Added Greet

--- src/main/java/org/example/Controller.java
+++ src/main/java/org/example/Controller.java
@@ -1,16 +1,32 @@
 package org.example;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.stereotype.Controller
 class Controller {
-	@GetMapping(value = {"/", "/public"})
-	public String publicPage() {
-		return "public";
-	}
+    @GetMapping(value = {"/", "/public"})
+    public String publicPage(Model model, Authentication authentication) {
+        model.addAttribute("name", getName(authentication));
+        return "public";
+    }
 
-	@GetMapping("/private")
-	public String privatePage() {
-		return "private";
-	}
+    @GetMapping("/private")
+    public String privatePage(Model model, Authentication authentication) {
+        // TODO 4. Greet logged in user
+        model.addAttribute("name", getName(authentication));
+        return "private";
+    }
+
+    private String getName(Authentication authentication) {
+        if (authentication == null) {
+            return "Stranger";
+        }
+        if (authentication.getPrincipal() instanceof OidcUser oidcUser) {
+            return oidcUser.getEmail();
+        }
+        return authentication.getName();
+    }
 }
---
+++ src/main/java/org/example/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+// TODO 3. add security configuration
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(request -> {
+                    request.requestMatchers("/").permitAll();
+                    request.requestMatchers("/public").permitAll();
+                    request.requestMatchers("/error").permitAll();
+                    request.requestMatchers("/css/*").permitAll();
+                    request.requestMatchers("/favicon.svg").permitAll();
+                    request.anyRequest().authenticated(); // default to login needed
+                })
+                .formLogin(withDefaults()) // adds login form
+                .oauth2Login(withDefaults()) // adds SSO form
+                .logout((logout) -> logout.logoutSuccessUrl("/")) // redirect to start page after logout
+                .build();
+    }
+}
